51cac3a012616eadc8cc4cd38278449dff5c835a
JiangYu <lnsyyj@hotmail.com>
2018-08-15 18:28:00 +0800

    update schedule_rbd.sh

--- rbd_test_yaml/schedule_rbd.sh
+++ rbd_test_yaml/schedule_rbd.sh
@@ -8,10 +8,13 @@ function activate() {
 	source /home/teuthworker/src/teuthology_master/virtualenv/bin/activate
 }
 
-#su - paddles -c "source /home/teuthworker/src/teuthology_master/virtualenv/bin/activate"
 activate
+pushd /home/teuthology/src/teuthology_master/rbd_test_yaml/
+
 teuthology-schedule --name "${DATA}-${SUITE}" verify_pool.yaml
 teuthology-schedule --name "${DATA}-${SUITE}" import_export.yaml
 teuthology-schedule --name "${DATA}-${SUITE}" diff_continuous.yaml
 teuthology-schedule --name "${DATA}-${SUITE}" copy.yaml
 teuthology-schedule --name "${DATA}-${SUITE}" concurrent.yaml
+
+popd
