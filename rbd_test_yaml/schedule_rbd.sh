8b59def6477dfd8af1d37db36baf9c52ef37c81e
JiangYu <lnsyyj@hotmail.com>
2018-08-16 11:07:35 +0800

    Add yaml to generate template
    Signed-off-by: JiangYu <lnsyyj@hotmail.com>

--- rbd_test_yaml/schedule_rbd.sh
+++ rbd_test_yaml/schedule_rbd.sh
@@ -3,7 +3,10 @@ set -x
 
 DATA=$(date +%Y-%m-%d)
 SUITE="RBD"
-echo ${DATA}
+#FILE_LIST=(import_export.yaml verify_pool.yaml diff_continuous.yaml copy.yaml concurrent.yaml)
+
+FILE_LIST=(import_export.yaml verify_pool.yaml)
+
 function activate() {
 	source /home/teuthworker/src/teuthology_master/virtualenv/bin/activate
 }
@@ -11,10 +14,15 @@ function activate() {
 activate
 pushd /home/teuthology/src/teuthology_master/rbd_test_yaml/
 
-teuthology-schedule --name "${DATA}-${SUITE}" verify_pool.yaml
-teuthology-schedule --name "${DATA}-${SUITE}" import_export.yaml
-teuthology-schedule --name "${DATA}-${SUITE}" diff_continuous.yaml
-teuthology-schedule --name "${DATA}-${SUITE}" copy.yaml
-teuthology-schedule --name "${DATA}-${SUITE}" concurrent.yaml
+for file_name in ${FILE_LIST[@]}
+do
+	teuthology-schedule --name "${DATA}-${SUITE}" ${file_name}
+done
+
+#teuthology-schedule --name "${DATA}-${SUITE}" import_export.yaml
+#teuthology-schedule --name "${DATA}-${SUITE}" verify_pool.yaml
+#teuthology-schedule --name "${DATA}-${SUITE}" diff_continuous.yaml
+#teuthology-schedule --name "${DATA}-${SUITE}" copy.yaml
+#teuthology-schedule --name "${DATA}-${SUITE}" concurrent.yaml
 
 popd
