f596c0a753b5756be8fed146cad4b980b8436b08
JiangYu <lnsyyj@hotmail.com>
2018-08-16 16:44:57 +0800

    add yamls
    Signed-off-by: JiangYu <lnsyyj@hotmail.com>

---
+++ rbd_test_yaml/sendmail.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+
+DATA=$(date +%Y-%m-%d)
+SUITE="RBD"
+echo ${DATA}
+function activate() {
+	source /home/teuthworker/src/teuthology_master/virtualenv/bin/activate
+}
+
+activate
+teuthology-results --email yujiang2@lenovo.com --timeout 1 --archive-dir /home/teuthworker/archive --name "${DATA}-RBD" --subset rbd --seed 1
+#teuthology-results --email yujiang2@lenovo.com --timeout 1 --archive-dir /home/teuthworker/archive --name "${DATA}-RADOS" --subset rados --seed 1
