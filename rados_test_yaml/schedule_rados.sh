0602dd4e755e994413b96af195a5aa215cb798a8
JiangYu <lnsyyj@hotmail.com>
2018-09-19 13:50:51 +0800

    update

---
+++ rados_test_yaml/schedule_rados.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -x
+
+DATA=$(date +%Y%m%d)
+SUITE="RADOS"
+
+FILE_LIST=(load-gen-big.sh.yaml load-gen-mix.sh.yaml load-gen-mix-small-long.sh.yaml load-gen-mix-small.sh.yaml load-gen-mostlyread.sh.yaml stress_watch.sh.yaml test_cache_pool.sh.yaml test_pool_quota.sh.yaml test_python.sh.yaml test_rados_timeouts.sh.yaml test_rados_tool.sh.yaml test.sh.yaml test_tmap_to_omap.sh.yaml test-upgrade-v11.0.0.sh.yaml)
+#FILE_LIST=(clone.sh.yaml load-gen-big.sh.yaml load-gen-mix.sh.yaml load-gen-mix-small-long.sh.yaml load-gen-mix-small.sh.yaml load-gen-mostlyread.sh.yaml stress_watch.sh.yaml test_alloc_hint.sh.yaml test_cache_pool.sh.yaml test_hang.sh.yaml test_pool_quota.sh.yaml test_python.sh.yaml test_rados_timeouts.sh.yaml test_rados_tool.sh.yaml test.sh.yaml test_tmap_to_omap.sh.yaml test-upgrade-v11.0.0.sh.yaml)
+
+function activate() {
+	source /home/teuthworker/src/teuthology_master/virtualenv/bin/activate
+}
+
+activate
+pushd /home/teuthology/src/teuthology_master/rados_test_yaml/
+
+for file_name in ${FILE_LIST[@]}
+do
+	teuthology-schedule --name "${DATA}-${SUITE}" ${file_name}
+done
+
+#teuthology-schedule --name "${DATA}-${SUITE}" verify_pool.yaml
+
+popd
