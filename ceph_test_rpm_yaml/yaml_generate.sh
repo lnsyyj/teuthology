dc46145fa6bb7f20df4d9a43dbd03d0593e8a298
JiangYu <lnsyyj@hotmail.com>
2018-10-23 18:32:19 +0800

    update
    Signed-off-by: JiangYu <lnsyyj@hotmail.com>

---
+++ ceph_test_rpm_yaml/yaml_generate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#FILE_LIST=(cli_generic.sh concurrent.sh diff_continuous.sh diff.sh huge-tickets.sh image_read.sh import_export.sh issue-20295.sh journal.sh kernel.sh krbd_data_pool.sh krbd_exclusive_option.sh krbd_fallocate.sh krbd_stable_pages_required.sh map-snapshot-io.sh map-unmap.sh merge_diff.sh notify_master.sh notify_slave.sh permissions.sh qemu_dynamic_features.sh qemu-iotests.sh qemu_rebuild_object_map.sh rbd-ggate.sh rbd_mirror_ha.sh rbd_mirror_helpers.sh rbd_mirror.sh rbd_mirror_stress.sh rbd-nbd.sh read-flags.sh run_devstack_tempest.sh set_ro.py simple_big.sh smalliobench.sh test_admin_socket.sh test_librbd_api.sh test_librbd_python.sh test_librbd.sh test_lock_fence.sh test_rbdmap_RBDMAPFILE.sh test_rbd_mirror.sh verify_pool.sh)
+
+FILE_LIST=(ceph_omapbench ceph_perf_local)
+
+COUNT=1
+for file_name in ${FILE_LIST[@]}
+do
+	cp yaml_template ${file_name}.yaml
+	sed -i "s/yaml_template/${file_name}/g" ${file_name}.yaml
+        echo ${COUNT}
+	COUNT=$((${COUNT}+1))
+done
