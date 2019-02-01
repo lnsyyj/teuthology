1a18e06258d83a871cc1bd4c022e1f92be39f7e2
JiangYu <lnsyyj@hotmail.com>
2019-02-01 15:59:26 +0800

    update fs
    Signed-off-by: JiangYu <lnsyyj@hotmail.com>

--- 13_fs_test_yaml/schedule_fs.sh
+++ 13_fs_test_yaml/schedule_fs.sh
@@ -2,7 +2,7 @@
 set -x
 
 DATA=$(date +%Y%m%d)
-SUITE="3.0-fs"
+SUITE="3.0-FS"
 
 FILE_LIST=(direct_io.py.yaml dirfrag.sh.yaml filelock_deadlock.py.yaml filelock_interrupt.py.yaml i_complete_vs_rename.sh.yaml mkpool_layout_vxattrs.sh.yaml rstats.sh.yaml trivial_sync.sh.yaml xattrs.sh.yaml quota.sh.yaml snap-rm-diff.sh.yaml snaptest-1.sh.yaml snaptest-2.sh.yaml snaptest-authwb.sh.yaml snaptest-capwb.sh.yaml snaptest-dir-rename.sh.yaml snaptest-double-null.sh.yaml snaptest-estale.sh.yaml snaptest-git-ceph.sh.yaml snaptest-hardlink.sh.yaml snaptest-intodir.sh.yaml snaptest-multiple-capsnaps.sh.yaml snaptest-parents.sh.yaml snaptest-realm-split.sh.yaml snaptest-snap-rename.sh.yaml snaptest-snap-rm-cmp.sh.yaml snaptest-upchildrealms.sh.yaml snaptest-xattrwb.sh.yaml)
 
